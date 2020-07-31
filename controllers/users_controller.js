5d4814d4c35d3554fed1f7a7fabecaed1091e368
Bhaskar Acharya <bhaskar_acharya1999@rediffmail.com>
2020-07-31 23:22:25 +0530

    After passport js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,6 +13,10 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     
@@ -21,6 +25,10 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     
@@ -53,5 +61,14 @@ module.exports.create = function(req, res){
 
 // sign in the user as a session
 module.exports.createSession = function(req, res){
-    //TODO later
+    //TODO later (Done)
+    return res.redirect('/');
+
+}
+
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+
+    return res.redirect('/');
 }
